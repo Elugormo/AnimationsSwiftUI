ed804374faf7fc7d61abcc212d940aa6a0c28080
Johnny <romanomess@gmail.com>
2020-08-21 08:21:45 +0300

    Initial Commit

---
+++ HeroAnimations/HeroAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroAnimationsApp.swift
+//  HeroAnimations
+//
+//  Created by Johnny on 21.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct HeroAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
